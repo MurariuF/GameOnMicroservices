f86d7aee223b75a002ae1bdc992a7ced8fb52358
MurariuF <murariu.flavius@gmail.com>
2024-04-24 15:49:12 +0300

    Created common project, configure it and refactor and fix dependency in Catalog

--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966f8d85530235a1caf757028c20bf067230c928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474df539191a173d6a35b02c114f57b94d6819c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Play.Common/src/Play.Common/MongoDB/MongoRepository.cs
+++ Play.Common/src/Play.Common/MongoDB/MongoRepository.cs
@@ -1,10 +1,10 @@
 using MongoDB.Driver;
-using Play.Catalog.Service.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace Play.Catalog.Service.Repositories
+namespace Play.Common.MongoDB
 {
     public class MongoRepository<T> : IRepository<T> where T : IEntity
     {
@@ -21,10 +21,21 @@ namespace Play.Catalog.Service.Repositories
             return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
         }
 
+        public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await dbCollection.Find(filter).ToListAsync();
+        }
+
         public async Task<T> GetAsync(Guid id)
         {
             FilterDefinition<T> filter = filterBuilder.Eq(entity => entity.Id, id);
             return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+
         }
 
         public async Task CreateAsync(T entity)
@@ -53,5 +64,6 @@ namespace Play.Catalog.Service.Repositories
             FilterDefinition<T> filter = filterBuilder.Eq(entity => entity.Id, id);
             await dbCollection.DeleteOneAsync(filter);
         }
+
     }
 }
