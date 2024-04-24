2b40ec378ce99a799305c33ca9145da277447428
MurariuF <murariu.flavius@gmail.com>
2024-04-24 19:01:09 +0300

    added implementantion to communicate between catalog and inventory to receive all infos that are needed

--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0f87c58068babac72bcffea2d2c51c649d860e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43687151524bc0a601592b580c82787c5ee80b69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
