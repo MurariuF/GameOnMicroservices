fc0f87c58068babac72bcffea2d2c51c649d860e
MurariuF <murariu.flavius@gmail.com>
2024-04-24 16:13:31 +0300

    Create project for infrastucture and create docker compose file with mongo

--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474df539191a173d6a35b02c114f57b94d6819c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86d7aee223b75a002ae1bdc992a7ced8fb52358")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
