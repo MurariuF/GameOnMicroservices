b03f31274bce6380a4873a9e77b90719101311cc
MurariuF <murariu.flavius@gmail.com>
2024-04-25 13:53:15 +0300

    added open circuit when time and retries are too many

--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71f3c2d9e81f875158bb0cc9770ab5b9c2edcfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4a32ab3a229e9a1e38199ccc14d12b5080702e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
